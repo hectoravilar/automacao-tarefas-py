d8776f6e9e405375e0c81fdb1427e3948a86b3c1
Hector Avila <hectoravllr@gmail.com>
2025-09-16 14:37:13 -0300

    Inserindo base de dados e Cadastrando Produtos

--- auto.py
+++ auto.py
@@ -1,15 +1,15 @@
 # importando bibliotecas
 import pyautogui
-import time 
+import time
 import webbrowser
 
 pyautogui.PAUSE = 0.5
 
-# Abrindo navegador e entrando no site 
+# Abrindo navegador e entrando no site
 webbrowser.open('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
 time.sleep(2)
 
-# Fazendo Login 
+# Fazendo Login
 # preecher email
 pyautogui.click(x=2727, y=-1059) #Clicando no campo de email
 pyautogui.write('hectoravllr@gmail.com') #Escrevendo email
@@ -28,38 +28,49 @@ import pandas
 
 tabela = pandas.read_csv("produtos.csv")
 
-# Cadastrando produtos
-pyautogui.click(x=2802, y=-1237)
+# Cadastrando 1 Produto
 
-# Cadastrando produtos
-codigo = "MOLO000251"
-pyautogui.write(codigo)
-pyautogui.press('tab')
 
-marca = "Logitech"
-pyautogui.write(marca)
-pyautogui.press('tab')
+for linha in tabela.index:
+    pyautogui.click(x=2802, y=-1237)
 
-tipo =  "Mouse"
-pyautogui.write(tipo)
-pyautogui.press('tab')
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+    pyautogui.press('tab')
 
-categoria = "1"
-pyautogui.write(categoria)
-pyautogui.press('tab')
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+    pyautogui.press('tab')
 
-preco_unitario = "25.95"
-pyautogui.write(preco_unitario)
-pyautogui.press('tab')
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+    pyautogui.press('tab')
 
-custo = "6.50"
-pyautogui.write(custo)
-pyautogui.press('tab')
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(categoria)
+    pyautogui.press('tab')
 
-obs = ""
-pyautogui.write(obs)
-pyautogui.press('tab')
-pyautogui.press('enter')
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
+    pyautogui.press('tab')
+
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)
+    pyautogui.press('tab')
+
+    obs = str(tabela.loc[linha, "obs"])
+
+    if obs != "nan":
+        pyautogui.write(obs)
+
+
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+    pyautogui.scroll(10000)
+
+
+
+
 
 
 
