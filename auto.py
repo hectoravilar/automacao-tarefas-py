f22a5b9ba1b60ee80047975fa88d9acea890ef6d
Hector Avila <hectoravllr@gmail.com>
2025-09-16 09:38:25 -0300

    cadastrando produtos

--- auto.py
+++ auto.py
@@ -23,4 +23,45 @@ pyautogui.write('123456') #Escrevendo senha
 pyautogui.press('tab') #Selecionando o botao de logar
 pyautogui.press("enter") #Clicando no botao de logar
 
+#importando base de dados
+import pandas
+
+tabela = pandas.read_csv("produtos.csv")
+
+# Cadastrando produtos
+pyautogui.click(x=2802, y=-1237)
+
+# Cadastrando produtos
+codigo = "MOLO000251"
+pyautogui.write(codigo)
+pyautogui.press('tab')
+
+marca = "Logitech"
+pyautogui.write(marca)
+pyautogui.press('tab')
+
+tipo =  "Mouse"
+pyautogui.write(tipo)
+pyautogui.press('tab')
+
+categoria = "1"
+pyautogui.write(categoria)
+pyautogui.press('tab')
+
+preco_unitario = "25.95"
+pyautogui.write(preco_unitario)
+pyautogui.press('tab')
+
+custo = "6.50"
+pyautogui.write(custo)
+pyautogui.press('tab')
+
+obs = ""
+pyautogui.write(obs)
+pyautogui.press('tab')
+pyautogui.press('enter')
+
+
+
+
 
